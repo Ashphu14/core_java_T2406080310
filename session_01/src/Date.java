44f2beff1999d4fe460509cdd41830e4fe9ada90
Ashish Phulari <Ashuphulari1421@gmail.com>
2024-02-13 11:12:23 +0530

    Final Commit

---
+++ session_01/src/Date.java
@@ -0,0 +1,18 @@
+
+public class Date {
+	private int dd, mm, yy;
+	public void inItDate() {
+		dd=mm=yy;
+		
+	}
+	public void setDate(int d,int m, int y) {
+		dd= d;
+		mm= m;
+		yy= y;
+	}
+	public void displaydate() {
+		System.out.println("date: ["+dd+"/"+mm+"/"+yy+"]");
+	}
+	public static void main
+
+}
